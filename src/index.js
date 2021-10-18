c3c0b062fdabdd2c18411b6d7a3ffe28bde07d2c
Leandro Kaul <leandrokaul@hotmail.com>
2021-10-18 05:59:29 -0300

    Se limpio codigo y se dejo el correspondiente al desafio (aun falta limpiar algunas cosas de denormlz). Se configuro Hbs correctamente

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import config from './config';
+import server from './services/server'
+import { connectDb } from './services/db';
+
+const puerto=config.PORT;
+
+connectDb().then(() => {
+    console.log('DB CONECTADA');
+    server.listen(puerto, () => console.log("Server up "+puerto))
+  
+    server.on('error', (error) => console.log(`Error en servidor: ${error}`));
+  });
+
+
+
